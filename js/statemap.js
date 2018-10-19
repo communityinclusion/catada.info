1ffd99fdf37fc8087d323e8fb570f7c20119fa6b
Paul Foos <paul.foos@umb.edu>
2018-10-19 11:21:56 -0400

    fixes to state info page state selection

--- js/statemap.js
+++ js/statemap.js
@@ -1,5 +1,30 @@
 jQuery.noConflict();
  jQuery(document).ready(function() {
+    jQuery("#jumpState").submit(function(event){
+        event.preventDefault();
+    });
+        if (window.location.href.indexOf("state.html") > -1) {
+        var stateHash = window.location.hash.substr(1) ? window.location.hash.substr(1) : '' ;
+        console.log('State page');
+        
+        if(stateHash.length > 0) {
+            console.log(stateHash);
+        var url = '#' + stateHash;
+        window.location = url; 
+        jQuery('#stateHolder > div').removeClass("stateDisplay");
+        jQuery('#stateHolder > div').addClass("phpfix");
+        jQuery('#stateHolder ' + url).addClass("stateDisplay");
+        jQuery('#stateHolder ' + url).removeClass("phpfix");
+        jQuery('html, body').animate({
+                    scrollTop: jQuery("#stateHolder").offset().top
+                    }, 200);
+                }
+                
+     
+
+
+        }
+    
     
 
      jQuery(".stateGroup").mouseenter(function() {
@@ -11,21 +36,43 @@ jQuery.noConflict();
 
 
 
- jQuery(".stateGroup").click(
+    jQuery(".stateGroup").click(
 
-function()
-{
-    var stateId = jQuery(this).attr('id').substring(4);
-    var url = '#' + stateId;
-   window.location = url;
-    jQuery('#stateHolder > div').removeClass("stateDisplay");
-    jQuery('#stateHolder > div').addClass("phpfix");
-   jQuery('#stateHolder #' + stateId).addClass("stateDisplay");
-   jQuery('#stateHolder #' + stateId).removeClass("phpfix");
-  jQuery('html, body').animate({
+    function()
+    {
+        var stateId = jQuery(this).attr('id').substring(4);
+        var url = '#' + stateId;
+         window.location = url;
+        jQuery('#stateHolder > div').removeClass("stateDisplay");
+        jQuery('#stateHolder > div').addClass("phpfix");
+        jQuery('#stateHolder #' + stateId).addClass("stateDisplay");
+        jQuery('#stateHolder #' + stateId).removeClass("phpfix");
+        jQuery('html, body').animate({
                         scrollTop: jQuery("#stateHolder").offset().top
                     }, 200);
-   return false;
-});
+        return false;
+    });
+
+    jQuery("#chooseState").click(function(){
+  
+    
+
+    
+        var selectState = jQuery('#jumpState #stateSelect').find(":selected").val();
+        console.log('selected: ' + selectState);
+        var stateId = selectState;
+        var url = '#' + stateId;
+         window.location = url;
+        jQuery('#stateHolder > div').removeClass("stateDisplay");
+        jQuery('#stateHolder > div').addClass("phpfix");
+        jQuery('#stateHolder #' + stateId).addClass("stateDisplay");
+        jQuery('#stateHolder #' + stateId).removeClass("phpfix");
+        jQuery('html, body').animate({
+                        scrollTop: jQuery("#stateHolder").offset().top
+                    }, 200);
+        return false;
+
+    });
 
 });
+
